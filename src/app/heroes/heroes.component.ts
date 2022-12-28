25536a574413c5507e3a4fc1b7381bc647ac8f24
Alisée Nollevalle <l.nollevalle@gmail.com>
2022-12-28 17:56:48 +0100

    hero service added and modification const+onInit

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,12 +9,17 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private heroService: HeroService) {}
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
-  heroes = HEROES;
+  heroes: Hero[] = [];
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
 
   hero: Hero = {
     id: 1,
